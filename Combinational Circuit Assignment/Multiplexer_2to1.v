884fef36dd0ea7f26a12a32f2b1a6c43ce3ac141
Suraj Prajapati <117726824+surajPrajapati-rgb@users.noreply.github.com>
2024-02-26 18:21:00 +0530

    this was comlex codes

--- Combinational Circuit Assignment/Multiplexer_2to1.v
+++
@@ -1,7 +0,0 @@
-module multiplexer(a,b,sel,c);
-input a,b,sel;
-output c;
-
-assign c = (sel) ? b:a;
-
-endmodule
--- Combinational Circuit Assignment/encoder_8to3.v
+++
@@ -1,9 +0,0 @@
-module encoder(a7,a6,a5,a4,a3,a2,a1,a0,y2,y1,y0);
-input a7,a6,a5,a4,a3,a2,a1,a0;
-output y2,y1,y0;
-
-assign y2 = a7 | a6 | a5 | a4;
-assign y1 = a7 | a6 | a3 | a2;
-assign y0 = a7 | a5 | a3 | a1;
-
-endmodule
